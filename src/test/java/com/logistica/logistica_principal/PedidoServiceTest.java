f15221c7ea22116ddad20ad6363d2979fa17356c
oriana02 <or.solorzano@duocuc.cl>
2025-06-04 22:25:52 -0400

    se agrego docker-compuse

---
+++ src/test/java/com/logistica/logistica_principal/PedidoServiceTest.java
@@ -0,0 +1,18 @@
+package com.logistica.logistica_principal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+
+import com.logistica.logistica_principal.service.PedidoService;
+
+public class PedidoServiceTest {
+    @InjectMocks
+    private PedidoService pedidoservice;
+
+    @BeforeEach
+    void setup(){
+        
+    }
+
+
+}
